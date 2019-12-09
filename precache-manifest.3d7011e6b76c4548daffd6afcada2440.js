96c1d5960b794b9e92100fbe832fe80e4c175922
Alexa King <kingjalexa@gmail.com>
2019-12-09 09:54:10 -0500

    Updates

---
+++ precache-manifest.3d7011e6b76c4548daffd6afcada2440.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7e3d21f8ad0085f88112d68e097200",
+    "url": "/alexaking.xyz/index.html"
+  },
+  {
+    "revision": "b7e2ffc8c559d61df06b",
+    "url": "/alexaking.xyz/static/css/2.f0c0c278.chunk.css"
+  },
+  {
+    "revision": "38a743c5413d54210698",
+    "url": "/alexaking.xyz/static/css/main.c83ce78d.chunk.css"
+  },
+  {
+    "revision": "b7e2ffc8c559d61df06b",
+    "url": "/alexaking.xyz/static/js/2.dfbfa7b9.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/alexaking.xyz/static/js/2.dfbfa7b9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38a743c5413d54210698",
+    "url": "/alexaking.xyz/static/js/main.a6aaa0ed.chunk.js"
+  },
+  {
+    "revision": "bbb4b86da30eed7f7e6c",
+    "url": "/alexaking.xyz/static/js/runtime-main.69b6be56.js"
+  }
+]);
